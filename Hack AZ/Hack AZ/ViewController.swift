aae69365e26d5d1b66487ef7b38353cf07db830c
Cody Deeran <cdeeran2@gmail.com>
2017-10-03 23:48:20 -0700

    Added - Hack AZ Banner, UITableView

--- Hack AZ/Hack AZ/ViewController.swift
+++ Hack AZ/Hack AZ/ViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    let cellContent = ["Schedule","Map","Activities", "Events", "Mentors" ,"Sponsors"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellContent.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "mainCell")
+        cell.contentView.backgroundColor = UIColor(red: CGFloat(164)/255.0, green: CGFloat(125)/255.0, blue: CGFloat(196)/255.0, alpha: 1.0)
+        cell.textLabel?.text = cellContent[indexPath.row]
+        cell.textLabel?.font = UIFont(name: "Arial", size:22.0)
+        cell.textLabel?.textColor = UIColor.white
+        return cell
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
